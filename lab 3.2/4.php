9ae34b2f06ea29452b041f5e4c12f2d049296810
Marishat Tasmim <123513995+Marishat@users.noreply.github.com>
2024-03-30 04:54:52 +0600

    Update 4.php

--- lab 3.2/4.php
+++ lab 3.2/4.php
@@ -18,18 +18,15 @@
 </body>
 </html>
 
-<?php
-    
-        $selectedGender = $_POST['gender'] ?? '';
 
-        if (!empty($selectedGender)) 
-		{
-            header("Location: 5.php");
-            exit();
-        } 
-		else 
-		{
-            echo "Please select a gender.";
-        }
-    
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $gender = isset($_POST['gender']) ? $_POST['gender'] : null;
+
+    if (!empty($gender)) {
+        header("Location: 5.php");
+        exit();
+    }
+}
 ?>
